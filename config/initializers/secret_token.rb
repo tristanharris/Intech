2c275de7ff81a5cc748f7c31b1bb51a4facf3c0f
Tristan Harris <tris.dev@pumpcottage.me.uk>
2011-02-04 19:33:18 +0000

    Default rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Intech::Application.config.secret_token = 'aac1e01e5dd2f0575fa404174ef6cf869d7de92c35c6db33239dbbea3e1378ea850fd8110d4556a1cd76d3b73c7ed96e94b32c7f87cc0a009bbb603daced0610'
