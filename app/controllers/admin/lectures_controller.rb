d5a75fd2fee2bd89ef78dddc9f2641bffdd90409
Tristan Harris <tris.dev@pumpcottage.me.uk>
2011-06-06 21:30:31 +0100

    Edit lectures

--- app/controllers/admin/lectures_controller.rb
+++ app/controllers/admin/lectures_controller.rb
@@ -11,10 +11,24 @@ class Admin::LecturesController < ApplicationController
     render :edit
   end
 
+  def edit
+    @lecture = Lecture.find(params[:id])
+    render :edit
+  end
+
   def create
     @lecture = Lecture.new(params[:lecture])
     if @lecture.save
-      redirect_to [:admin, Lecture]
+      redirect_to [:admin, Lecture], :notice => 'Lecture created'
+    else
+      render :edit
+    end
+  end
+
+  def update
+    @lecture = Lecture.find(params[:id])
+    if @lecture.update_attributes(params[:lecture])
+      redirect_to [:admin, Lecture], :notice => 'Lecture updated'
     else
       render :edit
     end
--- features/manage_events.feature
+++ features/manage_events.feature
@@ -15,3 +15,15 @@ Feature: Manage events
     And I press "Create"
     Then I should see "The Universe"
     And I should see "01/05/2011 18:30"
+
+  Scenario: Edit event
+    Given Lectures:
+      | title                   | time             | details     |
+      | The Universe            | 01/05/2011 18:30 | Interesting |
+    Given I am on the admin page
+    When I follow "The Universe"
+    When I fill in "Title" with "The Multiverse"
+    And I press "Update Lecture"
+    Then I should see "The Multiverse"
+    And I should see "01/05/2011 18:30"
+
