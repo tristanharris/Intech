f22d582680af517ba2b477d54f9550402e1555fd
Tristan Harris <tris.dev@pumpcottage.me.uk>
2011-06-12 14:39:51 +0100

    Ensure reqired fields

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -3,6 +3,8 @@ class Lecture < ActiveRecord::Base
   belongs_to :series
   delegate :title, :to => :series, :prefix => true, :allow_nil => true
 
+  validates_presence_of :title, :series, :time
+
   scope :active, lambda { where(:open => true).where('time > ?', Time.now) }
 
   def date
--- app/models/series.rb
+++ app/models/series.rb
@@ -2,6 +2,8 @@ class Series < ActiveRecord::Base
 
   has_many :lectures
 
+  validates_presence_of :title
+
   scope :active, where(:active => true)
 
   def active_lectures
